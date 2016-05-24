da6fb1c5e173668b2024c4be2eba6793fd959aed
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2016-05-24 16:27:44 -0700

    made 4.3 release 0 code for hamster adoption

---
+++ ruby/hamsters.rb
@@ -0,0 +1,34 @@
+puts "Hamsters Name:"
+hamster_name = gets.chomp
+
+puts "Volume Level (1-10)"
+volume_level = gets.chomp
+
+puts "Fur Color"
+fur_color = gets.chomp
+
+puts "Good Candidate for adoption (yes or no)"
+candidacy = gets.chomp
+
+puts "Estimated Age"
+est_age = gets.chomp
+
+
+
+def estimated_age(est_age)
+	if est_age = " "
+		est_age = "nil"
+	else est_age = est_age 
+end
+end
+
+puts estimated_age(est_age)
+
+
+
+
+
+
+
+
+
