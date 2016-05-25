54ac9266d1d068cd898d25d6d956af4f971e12c1
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2016-05-24 18:42:53 -0700

    release 0 and 1 4.4

---
+++ ruby/vampires.rb
@@ -0,0 +1,22 @@
+puts "What is your full name?"
+name = gets.chomp
+
+puts "How old are you?"
+age = gets.chomp.to_i
+
+puts "What year were you born?"
+birth_year = gets.chomp.to_i
+
+puts "Our cafeteria serves garlic bread! Should we order some for you?"
+garlic_bread_order = gets.chomp
+
+puts " Would you like to enroll in the company health insuracne?"
+insurance = gets.chomp
+
+
+wolves_like_sunshine = true
+wolves_like_garlic = true
+vampires_like_sunshine = false
+vampires_like_garlic = false
+
+wolves_like garlic
