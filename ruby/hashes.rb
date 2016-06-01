587ce47526d6ca200a41ebd3e6660cba4e80cee8
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2016-06-01 14:36:54 -0700

    psudeocoded the hw for 5.3

---
+++ ruby/hashes.rb
@@ -0,0 +1,12 @@
+#this is the out line of our hash key's 
+#client_name = {:name :age :number_children :decor_theme :likes_grey :like_brown :budget :street_address}
+
+#ask the user for all the values for each key term in the hash
+
+#convert the user input to the appropriate data type ie .to_s .to_i .to_a if needed
+
+#print the hash with key and values present for user to street_address
+
+#propmpt the user to update any keys one time, "none" will skip this step but if a key is called the program should allow for update, note: use stringmethod to convert a string to a symbol
+
+#print the latest version of the hash and exit program
