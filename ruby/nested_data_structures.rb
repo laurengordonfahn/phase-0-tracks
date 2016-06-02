337c5a78308c8a6bf270796df838f59327b70e25
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2016-06-02 11:56:35 -0700

    created my first nested data structure hope I did it right

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,11 @@
+nested_bookcase = {
+	book_shelf_1: {
+		books: ['acupuncture for dummmies': = {author: 'Lou Zee'}, 'moxabustion for dummies': = {author: 'Yang Ming'}, 'wild herbs': = {author: 'Helen Lavender'}]
+		things: {'salt lamp', 'frog figurine'}
+
+	}
+	book_shelf_2: {
+		books: ['bodice riper': = {author: 'Susan String'}, 'eligible': = {author: 'Curtis Sittenfeld'}]
+		things: {'trophy', 'magic eight ball'}
+	}
+}
