c666abfdacac7f5199a7989d4447acb2370acf73
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2016-06-04 13:21:31 -0700

    coded through the first step of step 2 in release 0 but having a strange output at end, need to fix

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -10,14 +10,25 @@ def name_changer1(name_to_be_altered)
 	name_arrayed = name_to_be_altered.split(' ')
 	p name_arrayed
 	# 	2) Doing a method that switches the first and second elements in the array
-	names_switched = name_arrayed.insert(0, name_arrayed.delete_at(1))
+	p name_arrayed.insert(0, name_arrayed.delete_at(1))
 end
 
-p name_changer1(name_to_be_altered)
+names_switched = name_changer1(name_to_be_altered)
+
 # 2) Goal: Change vowels to the next vowel "aeiou"
-	# - This requires
-		# 1) A method that identifies "aeiou" 
-		# 2) A method that then converts each identifed vowel and maps 
+		# 1) A method that breaks the name into each letter, array to a string to an array of letters
+def name_slicer(names_switched)
+	p stringed_switched_name = names_switched.join(' ')
+	p stringed_switched_name.split("")
+end
+
+name_split = name_slicer(names_switched)
+p "This is the product of the first step of step 2 #{name_split}"
+
+
+		# 2) A method that identifies "aeiou" 
+
+		# 3) A method that then converts each identifed vowel and maps 
 		# 	it to the next in an array of "aeiou"
 # 2B) Goal: Change all consonants to the next consonent in the alphabet
 	 # - This requires
