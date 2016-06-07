f56dcc772a58c08f295f5c760f795cc1f00753c2
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2016-06-06 22:51:12 -0700

    added speak method

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -7,3 +7,26 @@ class Puppy
 
 end
 
+dill = Puppy.new
+p dill.fetch("ball")
+
+
+class Puppy
+	def speak(number_of_times)
+		if number_of_times == 0
+			p " "
+		elsif number_of_times.to_i == 0
+			puts "The Value of your argument is not an integer."
+		elsif number_of_times = number_of_times.to_i
+			index = 0
+				until number_of_times == index
+					puts "Woof!"
+					index +=1
+				end
+		end
+	end
+end
+
+p dill.speak(3)
+
+
