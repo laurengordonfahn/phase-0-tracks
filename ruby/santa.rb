b827ed2878aec33063dcb09aa0eeb5298fff4f06
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2016-06-07 14:25:35 -0700

    release 1 finished, a number of instances of santas have been made

--- ruby/santa.rb
+++ ruby/santa.rb
@@ -28,16 +28,35 @@ class Santa
 
 
 
-	def initialize(name)
-		puts "Initializing Santa instance...#{name}."
-		@name = name
+	def initialize(name, gender, ethnicity)
+		puts "Initializing Santa instance...#{name}, #{gender}, #{ethnicity}."
+		@name = name.to_s
+		@gender = gender.to_s
+		@ethnicity = ethnicity.to_s
+		reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		age = 0
 
 	end
 end
 
 
 
-sarah = Santa.new("sarah")
-small_santa = Santa.new("small santa")
-sarah.speak(2)
-small_santa.eat_milk_and_cookies("Oatmeal Chocolate-Chip")
+# sarah = Santa.new("sarah")
+# small_santa = Santa.new("small santa")
+# sarah.speak(2)
+# small_santa.eat_milk_and_cookies("Oatmeal Chocolate-Chip")
+
+# array of santa names
+
+
+# an empty container for the santa instances
+santas = []
+example_names = ["Sarah", "Sting", "Sigmond", "Stella", "Stephen", "Sidney", "Singin"]
+example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+example_names.length.times do |i|
+  santas << Santa.new(example_names[i], example_genders[i], example_ethnicities[i])
+end
+
+
+
