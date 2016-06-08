6c8281c0211b11ad40ecebe2116521e516785fa8
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2016-06-08 14:14:54 -0700

    pseudo coded intentions for earplugs class

---
+++ ruby/solo.rb
@@ -0,0 +1,15 @@
+
+# class for ear plugs and the attributes will be all instance variable (@) material, deciple_level, color, number_in_warehouse
+# make an intialize method
+
+#make a def box_maker(num_desired, color_desired, deciple_desired, material_desired)
+		#I will need an if statement that goes throuh each variable to make sure it fits the criteria
+	 	#then a way to update the numer of ear plugs of that type in the warehouse
+		
+# make a deciple checker with variables sound and deciple_level
+	
+
+		
+# 	end
+
+# end
