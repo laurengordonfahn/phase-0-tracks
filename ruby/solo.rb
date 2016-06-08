7097e69b0f5bf20d2ab9776dff792f782a349a42
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2016-06-08 15:42:40 -0700

    relase 2 finished, need to complete release 3

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -6,39 +6,37 @@
 # class for ear plugs and the attributes will be all instance variable (@) material, deciple_level, color, number_in_warehouse
 # make an intialize method		
 class Ear_plug_types
-	def intialize(material, deciple_level, color, number_in_warehouse)
+	attr_accessor :number_in_warehouse
+	attr_reader :material, :deciple_level, :color
+
+	def initialize(material, deciple_level, color, number_in_warehouse, age)
 		@material = material
-		@deciple_level = deciple_level.to_i
+		@deciple_level = deciple_level
 		@color = color
-		@number_in_warehouse = number_in_warehouse.to_i
+		@number_in_warehouse = number_in_warehouse
+		@age = age 
 	end
 
 
 #make a def box_maker(num_desired, color_desired, deciple_desired, material_desired)
 		#I will need an if statement that goes throuh each variable to make sure it fits the criteria
 	 	#then a way to update the numer of ear plugs of that type in the warehouse
+	 	#turns out these dreams require me to have a class that collects all the instances of this class and then manages 
+	 	#them as they change, this was too much for this week so I did what you find below!
 		
-	def box_maker(num_desired, color_desired, deciple_desired, material_desired)
-		if number_in_warehouse - num_desired >= 0
-		&&
-		   color_desired == color
-		&&
-			deciple_desired <= deciple_level
-		&&
-			material_desired == material
-
-
-			number_in_warehouse = number_in_warehouse - number_desired
+	def age_checker
+		if @age > 3
+			puts "These have expired and may have degraded"
 		end
 	end
 
 
 # make a deciple checker with variables sound and deciple_level
 
-	def deciple_checker(sound, deciple_level)
-		noise_hash = { "classical instrument": "18 or greater", 
-			"electir instrument": "30 or greater"
-			"banned saw": "33 or greater"
+	def deciple_checker(sound)
+		noise_hash = { "classical instrument"=> "18 or greater", 
+			"electir instrument"=> "30 or greater",
+			"banned saw"=> "33 or greater"
 		}
 		if sound == "classical instrument" && deciple_level >= 18
 			puts "you are safe."
@@ -47,6 +45,18 @@ class Ear_plug_types
 		elsif sound == "banned saw" && deciple_level >= 33
 			puts "you are safe."
 		else
-			puts "you need a #{noise_hash[:sound]}"
+			puts "your earplug is not adiquite you need a #{noise_hash[sound]} deciple rated earplug."
 
 		end
+	end
+end
+
+p lot1 = Ear_plug_types.new("foam", 18, "purple", 250, 2)
+p lot2 = Ear_plug_types.new("clay", 33, "orange", 208, 4)
+
+p lot1.number_in_warehouse= 20
+p lot1
+lot2.deciple_checker("banned saw")
+
+def user_interface()
+	puts "This is a program to help you "
