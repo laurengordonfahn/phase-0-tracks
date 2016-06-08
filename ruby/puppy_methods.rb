824a4f9da0b56269d3660e1397fa53d3db23412b
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2016-06-08 11:26:51 -0700

    Finished Release 3 with help from Emmanuel, that was exhausting, but very helpful

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -1,32 +1,125 @@
-class Puppy
+# class Puppy
 
-  def fetch(toy)
-    puts "I brought back the #{toy}!"
-    toy
-  end
+#   def fetch(toy)
+#     puts "I brought back the #{toy}!"
+#     toy
+#   end
 
-end
+# end
 
-dill = Puppy.new
-p dill.fetch("ball")
+# dill = Puppy.new
+# p dill.fetch("ball")
 
 
-class Puppy
-	def speak(number_of_times)
-		if number_of_times == 0
-			p " "
-		elsif number_of_times.to_i == 0
-			puts "The Value of your argument is not an integer."
-		elsif number_of_times = number_of_times.to_i
-			index = 0
-				until number_of_times == index
-					puts "Woof!"
-					index +=1
-				end
-		end
+# class Puppy
+# 	def speak(number_of_times)
+# 		if number_of_times == 0
+# 			p " "
+# 		elsif number_of_times.to_i == 0
+# 			puts "The Value of your argument is not an integer."
+# 		elsif number_of_times = number_of_times.to_i
+# 			index = 0
+# 				until number_of_times == index
+# 					puts "Woof!"
+# 					index +=1
+# 				end
+# 		end
+# 	end
+# end
+
+# p dill.speak(3)
+
+# class Puppy
+
+#   def fetch(toy)
+#   puts "I brought back the #{toy}!"
+#   toy
+#   end
+
+#   def speak(num)
+#   p "Woof!" * num.to_i
+#   end
+
+#   def roll_over
+#   p "*rolls over*"
+#   end
+
+#   def dog_years(num)
+#   p num.to_i * 7
+#   end
+
+#   def shake_hands(num)
+#   p "*puts paw out*" * num.to_i
+#   end
+
+#   def initialize
+#   p "Initializing new puppy instance..."
+#   end
+
+# end
+
+# # DRIVER CODE
+
+# george = Puppy.new
+
+# george.fetch("stick")
+# george.speak(3)
+# george.roll_over
+# george.dog_years(3)
+# george.shake_hands(2)
+
+
+
+
+
+
+class Kittens
+	def initialize (name)
+		p "Intiializing a new kitten"
+		@name = name
 	end
+
+	def meow(num)
+		p "meow" * num.to_i
+	end
+
+	def sharpen_nails(furniture, num)
+		p "#{@name} sharpens nails on #{furniture} for #{num.to_i} seconds."
+	end
+
 end
 
-p dill.speak(3)
+kitten1 = Kittens.new("Billy-Bob")
+kitten1.meow(7)
+kitten1.sharpen_nails("couch", 10)
+
+
+kitten_collection = [ ]
+	index = 0
+	counter = 0
+	until kitten_collection.size > 49
+		index = counter
+		index = Kittens.new("kitten#{index}")
+		 
+		kitten_collection << index
+
+
+		counter +=1
+	end
+p kitten_collection
+
+kitten_collection.each do |kitten| 
+	kitten.meow(2)
+	kitten.sharpen_nails("couch", 3)
+
+	end
+
+ 
+
+
+
+
+
+
 
 
--- ruby/santa.rb
+++ ruby/santa.rb
@@ -1,9 +1,8 @@
 #Initalizeing class Santa
 class Santa
+	attr_reader :ethnicity
+	attr_accessor :gender, :age
 #Creating two instances of class Santa
-
-
-
 
 # Instance Methods for class Santa
 
@@ -16,15 +15,9 @@ class Santa
 	end
 
 
-
-
-
-
 	def eat_milk_and_cookies(type_of_cookie)
 		puts "That was a good #{type_of_cookie}."
 	end
-
-
 
 
 
@@ -33,30 +26,121 @@ class Santa
 		@name = name.to_s
 		@gender = gender.to_s
 		@ethnicity = ethnicity.to_s
-		reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
-		age = 0
+		@reindeer_ranking = ["Rudolph", "Dasher", "Dancer", "Prancer", "Vixen", "Comet", "Cupid", "Donner", "Blitzen"]
+		@age = 0
 
 	end
+
+#Release 2
+	def celebrate_birthday(age)
+		@age = age + 1
+		
+	end
+
+	def get_mad_at(reindeer_name)
+		@reindeer_ranking.delete(reindeer_name)
+		p @reindeer_ranking.push(reindeer_name)
+	end
+
+#This is noted because we added attr_ statements at the top of this code
+	# def gender= (new_gender)
+	# 	@gender = new_gender
+	# end
+
+	# def age
+	# 	@age
+	# end
+
+	# def ethnicity
+	# 	@ethnicity
+	# end
+
 end
 
 
-
+# driver code to test Release 0
 # sarah = Santa.new("sarah")
 # small_santa = Santa.new("small santa")
 # sarah.speak(2)
 # small_santa.eat_milk_and_cookies("Oatmeal Chocolate-Chip")
 
-# array of santa names
+#Release 1
+
+# an empty container for the santa instances
+# santas = []
+# example_names = ["Sarah", "Sting", "Sigmond", "Stella", "Stephen", "Sidney", "Singin"]
+# example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+# example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+# example_names.length.times do |i|
+#   santas << Santa.new(example_names[i], example_genders[i], example_ethnicities[i])
+# end
+
+
+#Release 2
+# class Reindeer
+#   def initialize(name)
+#     @name = name
+#   end
+# end
+
+# reindeer = Reindeer.new("Blitzen")
+# reindeer.name
+
+phil = Santa.new("Phil", "Maleish", "Caucasian")
+p phil.celebrate_birthday(4)
+phil.get_mad_at("Dancer")
+p phil.age
+p phil.ethnicity
+p phil.gender= ("Transcurious")
 
 
 # an empty container for the santa instances
-santas = []
-example_names = ["Sarah", "Sting", "Sigmond", "Stella", "Stephen", "Sidney", "Singin"]
-example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
-example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
-example_names.length.times do |i|
-  santas << Santa.new(example_names[i], example_genders[i], example_ethnicities[i])
-end
+def santa_generator(num)
+	@santas = []
+	example_names = ["Sarah", "Sting", "Sigmond", "Stella", "Stephen", "Sidney", "Singin"]
+	example_genders = ["agender", "female", "bigender", "male", "female", "gender fluid", "N/A"]
+	example_ethnicities = ["black", "Latino", "white", "Japanese-African", "prefer not to say", "Mystical Creature (unicorn)", "N/A"]
+	
+	num.times do |num|
+
+	new_santa = Santa.new(example_names.sample(example_names.length), example_genders.sample(example_genders.length), example_ethnicities.sample(example_ethnicities.length)) 
+				# name = example_names.rand(example_name.length)
+				# gender = example_genders.rand(example_genders.length)
+				# ethnicity = example_ethnicities.rand(example_ethnicities.length)
+				
+					 new_santa.age = (1..140).to_a.sample
+
+
+# Emmanual said these sage words otherwise there is no variable to accept the random number
+
+# [11:21]  
+# if you had another method that created age and called it before this that would work too
+
+# [11:21]  
+# basically, initialize sets all of the basic information for the class. What needs to exist for this class to exist
+	@santas << new_santa
+		# index = 0 do |num|
+		# 	until num == index 
+		# 		name = example_names.rand(example_name.length)
+		# 		gender = example_genders.rand(example_genders.length)
+		# 		ethnicity = example_ethnicities.rand(example_ethnicities.length)
+		# 		age = Random.rand(141)
+		# 	santas << Santa.new (name, gender,ethnicity)
+		# 	# name = example_names(rand(example_name.length)
+		# 	# gender = example_genders(rand(example_genders.length)
+		# 	# ethnicity = example_ethnicities(rand(example_ethnicities.length)
+		# 	# age = Random.rand(141)
+
+		# index += 1
+	
+	end
+end 
+
+santa_generator(5)
+p @santas[4].age
+
+
+
 
 
 
