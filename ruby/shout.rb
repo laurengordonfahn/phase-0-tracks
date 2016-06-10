807cf4879f647bea9fe978ceb4edc65965e3c828
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2016-06-10 12:32:55 -0700

    added two methods to the module and succesffully used driver code to run them in IRB

--- ruby/shout.rb
+++ ruby/shout.rb
@@ -1,3 +1,12 @@
 module Shout
-	#we'll put some methods here soon, but this code is fine for now!
+	def self.yell_angrily(words)
+		words + "!!!" + ":("
+	end
+
+	def self.yell_happily(words)
+		words + " all the live long daaaaaay!!!" + ":)"
+	end
 end
+
+p Shout.yell_angrily("what the")
+p Shout.yell_happily("yippy")
