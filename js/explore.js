2b8e88e5be23cde0d1f7125c9b473a31c658755b
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2016-06-13 18:16:25 -0700

    psudeo code

---
+++ js/explore.js
@@ -0,0 +1,19 @@
+// Goal 1 parapmeter class = > string
+	// 1) create a function with parmeter "Flip-Flop"
+	// 	console.log the parmeter so you know what it was when you started
+// Goal 2 reverse string
+	// 2) create a for loop 
+	// 	that sets a counter for that loop of i = to the length of the string
+	// 	that defines the condition for the loop i >= 0 
+	// 	var[i] = console.log  the results
+
+// Goal 3 printing the reveresed string in a line
+	// 3) creat a for loop for  adding the var[i]
+	// 	that sets the counter i= string.length
+	// 	that defines the condtion for the loop i>=10
+	// 	counter increment i+= string.length so only get the last out put
+
+
+
+	// 	for (var i =10; i >=0; i-=1) {
+	// console.log(i);
