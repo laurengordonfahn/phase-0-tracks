0c87257ee146564d6990df16b18c7c62e4966e9a
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2016-06-14 12:33:18 -0700

    nearly done but I need to tweek my driver code if statement

--- js/explore.js
+++ js/explore.js
@@ -10,7 +10,7 @@
 	// 	var[i] = console.log  the results
 	// pring the empty string at the end and then step 3 isn't needed
 
-// not needed with amended 2
+// not needed with amended 2 
 	//Goal 3 printing the reveresed string in a line
 	// 3) creat a for loop for  adding the var[i]
 	// 	that sets the counter i= string.length
@@ -24,20 +24,35 @@
 
 
 	//1)
-var string = "Flip-Flop";
+function reverse(string) {
+	var string;
 
-console.log(string);
+	console.log(string);
 
 
 	// 2) 
-flipped = ""
+	flipped = ""
 
-for (var i = string.length - 1; i >= 0; i--) {
+	for (var i = string.length - 1; i >= 0; i--) {
 
-	flipped = flipped + string[i]
+		flipped = flipped + string[i]
+
+	}
+	console.log(flipped);
+
 }
 
-console.log(flipped)
+reverse("Flip-Flop");
+
+var variable = 10
+
+for(var i = 1; i < variable; i++){
+	if ( i % 2.0 == 0){
+		reverse("Flip-Flop");
+	}
+} 
+
+
 
 	//3) Apparently this isn't needed when we do the above!
 // for (var i = string.length; i>=10; i += string.length) {
