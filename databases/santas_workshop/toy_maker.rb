803bf28cb13b0c28c60bd48a4ecc5cb147cb078c
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2016-06-22 21:19:55 -0700

    created three tables in my ruby document

--- databases/santas_workshop/toy_maker.rb
+++ databases/santas_workshop/toy_maker.rb
@@ -3,4 +3,10 @@
 # Object Relational Mapping (ORM)
 
 #require gems
-require ''
+require 'sqlite3'
+require 'faker'
+
+#create SQLITE3 database
+elves_data_base = SQLite3::Database.new("elves.db")
+children_data_base = SQLite3::Database.new("children.db")
+toys_data_base = SQLite3::Database.new("toys.db")
