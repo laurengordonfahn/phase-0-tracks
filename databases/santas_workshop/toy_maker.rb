a73a02c121b96d6c8ab99dd24e4f61371e47e0d2
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2016-06-22 21:33:12 -0700

    creating command for table creation

--- databases/santas_workshop/toy_maker.rb
+++ databases/santas_workshop/toy_maker.rb
@@ -10,3 +10,32 @@ require 'faker'
 elves_data_base = SQLite3::Database.new("elves.db")
 children_data_base = SQLite3::Database.new("children.db")
 toys_data_base = SQLite3::Database.new("toys.db")
+
+#delimiter
+create_table_elves = <<-SQL
+	CREATE TABLE elves(
+		id INTEGER PRIMARY KEY,
+		name VARCHAR(255),
+		toy_type_id INT,
+		number_toys INT,
+		FOREIGN KEY (toy_type_id) REFERENCES toys(id)
+		)
+SQL
+
+create_table_children = <<-SQL
+	CREATE TABLE children(
+		id INTEGER PRIMARY KEY,
+		name VARCHAR(255),
+		naught_nice BOOLEAN,
+		toy_type_id INT,
+		FOREIGN KEY (toy_type_id) REFERENCES toys(id)
+		)
+SQL
+
+create_table_toys = <<-SQL
+	CREATE TABLE toys(
+		id INTEGER PRIMARY KEY,
+		toy_name VARCHAR(255),
+		number_toys INT
+		)
+SQL
