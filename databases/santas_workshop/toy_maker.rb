c3601d2ae364e645c2583f28cef4ca9f964f0366
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2016-06-24 10:03:37 -0700

    nearly there?

--- databases/santas_workshop/toy_maker.rb
+++ databases/santas_workshop/toy_maker.rb
@@ -8,7 +8,7 @@ require 'faker'
 
 #create SQLITE3 database
 data_base = SQLite3::Database.new("santas_data_base.db")
-elves_data_base.results_as_hash = true
+data_base.results_as_hash = true
 
 #delimiter
 create_table_elves = <<-SQL
@@ -63,7 +63,7 @@ data_base.execute(create_table_toys)
 # 		puts "#{elf['name']} made #{elf['number_toys']} #{elf['toy_type_id']}"
 # 	end 
 
-# children = ata_base.execute("SELECT * FROM children")
+# children = data_base.execute("SELECT * FROM children")
 # puts children.class
 # p children
 
@@ -94,14 +94,35 @@ end
 	create_child(data_base, Faker::Name.name, Faker::Boolean.boolean, nil)
 end
 
-=begin
-child_data_base.each do |child|
-	if child['naught_nice']
-		 UPDATE child SET Faker::Superhero.name WHERE toy_type_id=null 
+
+# SAMPLE FOR MIRRORING
+# create_table_toys = <<-SQL
+# 	CREATE TABLE IF NOT EXISTS toys(
+# 		id INTEGER PRIMARY KEY,
+# 		toy_name VARCHAR(255),
+# 		number_toys INT
+# 		)
+# SQL
+
+
+
+# #create tables (if not already there)
+# data_base.execute(create_table_elves)
+
+child_toy = data_base.execute("SELECT naught_nice FROM children")
+	child_toy.each do |naught_nice|
+		if naught_nice
+	 		data_base.execute("UPDATE child SET Faker::Superhero.name WHERE toy_type_id=nil")
+		end
 	end
 
-end
-=end
+#elves = data_base.execute("SELECT * FROM elves")
+# 	elves.each do |elf|
+# 		puts "#{elf['name']} made #{elf['number_toys']} #{elf['toy_type_id']}"
+# 	end
+
+#end
+
 
 
 def create_toy(data_base, toy_name, number_toys) 
@@ -112,34 +133,47 @@ end
 p data_base.execute("SELECT * FROM toys")
 
 
-=begin
-	toys_type = toys_data_base.execute("SELECT id FROM toys")
+
+	toys_type = data_base.execute("SELECT id FROM toys")
 
 	toys_type.each do |toy_id|
-		# SQL:
-		# SELECT SUM(number_toys) AS toys_count FROM elves WHERE toy_type_id = ?
+		data_base.execute("SELECT SUM(number_toys) AS toys_count FROM elves WHERE toy_type_id = ?")
+	end
 
+	child_toy_type = data_base.execute ("SELECT id FROM toys")
+
+	child_toy_type.each do |toy_id|
+		data_base.execute("SELECT SUM(toy_type_id) AS child_toy_count FROM children WHERE toy_type_id = ?")
+	end
+	
+=begin
+
+	#get rid of this second half of above
+
+		children.toy_type_id FROM children
+		WHERE toys_id = children.toy_type_id;
+		SELECT SUM(toy_type_id) as toys_for_children FROM child_toy_count;")
+	end
+
+
+	#get rid of this
 		SELECT elves.toy_type_id, elves.number_toys
 		FROM elves, toys
 		WHERE toys.id = elves.toy_type_id
 		SELECT SUM(number_toys) as toys_count FROM toys_type;
 	end
 
-child_toy_count = toys_data_base.execute ("SELECT id FROM toys")
-child_toy_count.each do |toy_id|
-	SELECT children.toy_type_id FROM children
-	WHERE toys.id = children.toy_type_id;
-	SELECT SUM(toy_type_id) as toys_for_children FROM child_toy_count;
-end
+=end
 
-final_toy_sum = toys_data_base.execute("SELECT id FROM toys")
+
+final_toy_sum = data_base.execute("SELECT id FROM toys")
 final_toy_sum.each do |toy_id|
-	toys_count.to_i - toys_for_children.to_i
+	toys_count.to_i - child_toy_count.to_i
 end
 
 
 100.times do	
 	create_toy(toys_data_base, Faker::Superhero.name, final_toy_sum)
 end
-=end
+
 
