1846fa0cf4631fb8c70effdbf6c419a82df5cfb7
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2016-07-27 13:39:09 -0700

    finished

--- web_dev/client_side_js/add_js.js
+++ web_dev/client_side_js/add_js.js
@@ -1,3 +1,15 @@
-var list = document.getElementById("list");
+// var list = document.getElementById("list");
 
-list.insertAdjacentHTML('beforeend', '<p>For information on the stages of enlightenment click <a href="https://en.wikipedia.org/wiki/Four_stages_of_enlightenment">here</a></p>');
+// list.insertAdjacentHTML('beforeend', '<p>For information on the stages of enlightenment click <a href="https://en.wikipedia.org/wiki/Four_stages_of_enlightenment">here</a></p>');
+
+
+var link = document.getElementsByTagName("a");
+
+function addParagraph(event){
+	var list = document.getElementById("list");
+	list.insertAdjacentHTML('beforeend', '<p>For information on the stages of enlightenment click <a href="https://en.wikipedia.org/wiki/Four_stages_of_enlightenment">here</a></p>');
+}
+
+var list = document.getElementById("list");
+list.addEventListener("click", addParagraph);
+
