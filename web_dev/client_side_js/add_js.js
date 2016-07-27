0830f7c2d9f34f9649692366c5aabddf70ab10e4
Lauren Gordon-Fahn <laurengordonfahn@gmail.com>
2016-07-27 13:02:11 -0700

    files form Terrence

---
+++ web_dev/client_side_js/add_js.js
@@ -0,0 +1,3 @@
+var list = document.getElementById("list");
+
+list.insertAdjacentHTML('beforeend', '<p>For information on the stages of enlightenment click <a href="https://en.wikipedia.org/wiki/Four_stages_of_enlightenment">here</a></p>');
